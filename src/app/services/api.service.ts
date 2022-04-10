ac074149bfc8a86a2174445c0614552902fca795
Akhmadillo <akhmadillo.abduvakhidov@gmail.com>
2022-04-10 13:19:09 +0600

    crated json data and api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private _jsonUrl = 'assets/data/products.json'
+
+  constructor(private http: HttpClient) { }
+
+  getJson(): Observable<any> {
+    return this.http.get(this._jsonUrl)
+  }
+}
